541bc243c38ef4dc819328726abdbfb869e4aa66
iulian1976 <iulian_bulan@yahoo.com>
2020-08-21 14:48:36 +0200

    InterfaceController add isset

--- index.php
+++ index.php
@@ -2,44 +2,44 @@
 	<head>
 		<link rel="stylesheet" type="text/css" href="css/style.css">
 		<link href="https://fonts.googleapis.com/css?family=Open+Sans:600,700" rel="stylesheet">
-		<title>Interface</title>
+		<title>Interface Addition Two romane numbers</title>
 	</head>
 	<body>
 		<div id="container">
 			<div id="calculator">
-				<div id="result">
-					<div id="history">
-						<p id="history-value"></p>
-					</div>
-					<div id="output">
-						<p id="output-value"></p>
-					</div>
-				</div>
-				<div id="keyboard">
-                    <button class="number" id="1">I</button>
-                    <button class="number" id="2">II</button>
-                    <button class="number" id="3">III</button>
-                    <button class="number" id=""></button>
-					<button class="number" id="4">IV</button>
-					<button class="number" id="5">V</button>
-					<button class="number" id="6">VI</button>
-                    <button class="number" id=""></button>
-                    <button class="number" id="7">VII</button>
-                    <button class="number" id="8">VIII</button>
-                    <button class="number" id="9">IX</button>
-					<button class="operator" id="+">+</button>
-                    <button class="number" id="10">X</button>
-                    <button class="number" id="40">XL</button>
-                    <button class="number" id="50">L</button>
-					<button class="empty" id="empty">=</button>
-                    <button class="number" id="90">XC</button>
-                    <button class="number" id="100">C</button>
-                    <button class="number" id="400">CD</button>
-                    <button class="number" id=""></button>
-                    <button class="number" id="500">D</button>
-                    <button class="number" id="900">CM</button>
-                    <button class="number" id="1000">M</button>
-				</div>
+                <form action="InterfaceController.php" method="post">
+                        <div id="result">
+                            <div id="history">
+                                <p id="history-value"></p>
+                            </div>
+                            <div id="output">
+                                <p id="output-value"></p>
+                            </div>
+                        </div>
+
+                        <div id="keyboard">
+                            <input class="plusoperator" type='submit' name = 'plus'    value = '+' id="+">
+                            <input class="egaloperator" type='submit' name = 'egal'    value = '=' id="="><br>
+                            <input class="number" type='submit' name = 'op1'    value = 'I' id="1">
+                            <input class="number" type='submit' name = 'op1'    value = 'II' id="2">
+                            <input class="number" type='submit' name = 'op1'    value = 'III' id="3">
+                            <input class="number" type='submit' name = 'op1'    value = 'IV' id="4">
+                            <input class="number" type='submit' name = 'op1'    value = 'V' id="5"
+                            <input class="number" type='submit' name = 'op1'    value = 'VI' id="6">
+                            <input class="number" type='submit' name = 'op1'    value = 'VII' id="7">
+                            <input class="number" type='submit' name = 'op1'    value = 'VIII' id="8">
+                            <input class="number" type='submit' name = 'op1'    value = 'IX' id="9">
+                            <input class="number" type='submit' name = 'op1'    value = 'X' id="10">
+                            <input class="number" type='submit' name = 'op1'    value = 'XL' id="40">
+                            <input class="number" type='submit' name = 'op1'    value = 'L' id="50">
+                            <input class="number" type='submit' name = 'op1'    value = 'XC' id="90">
+                            <input class="number" type='submit' name = 'op1'    value = 'C' id="100">
+                            <input class="number" type='submit' name = 'op1'    value = 'CD' id="400">
+                            <input class="number" type='submit' name = 'op1'    value = 'D' id="500">
+                            <input class="number" type='submit' name = 'op1'    value = 'CM' id="900">
+                            <input class="number" type='submit' name = 'op1'    value = 'M' id="1000">
+                        </div>
+                </form>
 			</div>
 		</div>
 
